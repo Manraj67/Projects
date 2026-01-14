1eb0f761b883a850bd96fe926f4898a0af31998c
Manraj67 <baljinderd0702@gmail.com>
2026-01-13 21:01:24 -0500

    Add files via upload

---
+++ helloai.py
@@ -0,0 +1,11 @@
+print("hello I am Ai Chatbox. What's your name?" )
+a=input()
+print("Nice to meet you ",a)
+print("How are you feeling today?(good /bad):")
+mood=input()
+if mood=="good":
+    print("I'm glad to hear that!")
+elif mood=="bad":
+    print("I'm sorry to hear that. Hope things get better soon.")
+else :
+    print("I see. Sometimes it's hard to put feelings into words.")
