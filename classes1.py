7e412e5e301c878d9156e38b0ee7c559ef89a9a5
Manraj67 <baljinderd0702@gmail.com>
2026-05-20 19:17:36 -0400

    Add files via upload

---
+++ classes1.py
@@ -0,0 +1,5 @@
+class student:
+    grade = 10
+    print(f"Hi I am a student of grade {grade}")
+a = student()
+print(a.grade)
---
+++ classes2.py
@@ -0,0 +1,8 @@
+class vechicle:
+    a = 10
+    print("Hello")
+    def __init__(self, max_speed, milelage):
+        self.max_speed = max_speed
+        self.milelage = milelage
+z = vechicle(700, 50)
+print(z.a)
